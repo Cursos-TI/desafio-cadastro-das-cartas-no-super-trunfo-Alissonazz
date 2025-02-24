5f08b6137ec6cf3f52ea69c9620ca8f8cc40c420
Alisson Azevedo Silva Costa <aalissonazz@gmail.com>
2025-02-24 22:32:38 +0000

    new commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado [20];
+    char codigo;
+    char cidade [30];
+    float populacao, area, pib;
+    int turistico;
 
+    printf ("Siga agora para o cadastro da sua primeira carta: \n");
+    printf ("Qual estado? \n");
+    fgets (estado, 21, stdin);
+
+    printf ("Qual é o código do estado? \n");
+    scanf ("%s", &codigo);
+
+    printf ("Qual é a sua cidade? \n");
+    fgets (cidade, 31, stdin);
+
+    printf ("Diga a população: \n");
+    scanf ("%f", &populacao);
+
+    printf ("Diga quantos metros quadrados tem: \n");
+    scanf ("%f", &area);
+
+    printf ("Qual é o valor do pib? \n");
+    scanf ("%f", &pib);
+
+    printf ("Diga quantos pontos turisticos tem: \n");
+    scanf ("%d", &turistico);
+
+    printf ("A cidade de %s que fica no estado de %s tem o seguinte codigo: %s", cidade, estado, codigo);
+    printf ("A mesma cidade citada a cima tem %f habitantes, %f metros quadrados, um pib no valor de %f$ e %d pontos turisticos", populacao, area, pib, turistico);
     return 0;
 }
