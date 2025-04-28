c5d1d4d86e0cf3976e08eb27fcdd386bf645caa8
Alisson Azevedo Silva Costa <aalissonazz@gmail.com>
2025-04-28 19:57:53 +0000

    new commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,7 +15,7 @@ int main() {
     fgets (estado, 21, stdin);
 
     printf ("Qual é o código do estado? \n");
-    scanf ("%s", &codigo);
+    scanf ("%s", codigo);
 
     fgetc(stdin);
     printf ("Qual é a sua cidade? \n"); 
@@ -35,9 +35,9 @@ int main() {
     pibpc = (float) pib / populacao;
     densidadep = (float) populacao / area;
 
-    printf ("A cidade de %s, estado de %s, tem o seguinte codigo: %s \n", cidade, estado, codigo);
+    printf ("A cidade de %s que fica no estado de %s tem o seguinte codigo: %s \n", cidade, estado, codigo);
     printf ("A mesma cidade citada a cima tem %3.f habitantes, %3.f metros quadrados, um pib no valor de %2.f$ e %d pontos turisticos.\n", populacao, area, pib, turistico);
-    printf (" De acordo com esses dados, o pib per capita dessa cidade é de %2.f e a densidade populacional é de %2.f.\n", pibpc, densidadep);
+    printf ("De acordo com esses dados, o pib per capita dessa cidade é de %2.f e a densidade populacional é de %2.f.\n", pibpc, densidadep);
     printf ("Começaremos agora o cadastro da sua segunda carta:\n");
     
     fgetc(stdin);
@@ -45,7 +45,7 @@ int main() {
     fgets (estado2, 21, stdin);
 
     printf ("Qual é o código do estado? \n");
-    scanf ("%s", &codigo2);
+    fgets (codigo2, 26, stdin);
     
     fgetc(stdin);
     printf ("Qual é a sua cidade? \n"); 
@@ -61,12 +61,12 @@ int main() {
     scanf ("%f", &pib2);
 
     printf ("Diga quantos pontos turisticos tem: \n");
-    scanf ("%d", &turistico2);
+    scanf ( "%d", &turistico2);
     pibpc2 = (float) pib2 / populacao2;
     densidadep2 = (float) populacao2 / area2;
 
     printf (" A cidade de %s que fica no estado de %s tem o seguinte codigo: %s \n", cidade2, estado2, codigo2);
-    printf ("A mesma cidade citada a cima tem %f habitantes, %f metros quadrados, um pib no valor de %f$ e %d pontos turisticos.\n", populacao2, area2, pib2, turistico2);
+    printf ("A mesma cidade citada a cima tem %2.f habitantes, %2.f metros quadrados, um pib no valor de %2.f$ e %d pontos turisticos.\n", populacao2, area2, pib2, turistico2);
     printf (" De acordo com esses dados, o pib per capita dessa cidade é de %2.f e a densidade populacional é de %2.f.\n", pibpc2, densidadep2);
     
     return 0;
